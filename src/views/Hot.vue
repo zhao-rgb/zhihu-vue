6f32362f5521c702e8c28895b2bf875a9ac2be0d
zhao-rgb <1019919091@qq.com>
2020-02-02 21:30:43 +0800

    首页推荐初步

--- src/views/Hot.vue
+++ src/views/Hot.vue
@@ -1,16 +1,48 @@
 <template>
-	<div class="zh-container"><h3>热榜</h3></div>
+	<div>
+		<!-- 顶部tab导航 -->
+		<ul class="tab-title border-bottom">
+			<li v-for="(title, index) in tabTitle" @click="change(index)" :class="{ active: cur == index }" :key="index">{{ title }}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-	export default{
-			name:'hot',
-			data(){
-				return {};
-			},
-			created() {}
-	};
+export default {
+	name: 'hot',
+	data() {
+		return {
+			tabTitle: ['全站', '科学', '数码', '体育', '时尚', '影视', '校园', '汽车'],
+			category: ['total', 'science', 'digital', 'sport', 'fashion', 'film', 'school', 'car'],
+			tabContent: [[], [], [], [], [], [], [], []],
+			cur: 0
+		};
+	},
+	created() {}
+};
 </script>
 
 <style lang="scss" scoped>
+.tab-title {
+	display: flex;
+	align-items: center;
+	height: 63px;
+	padding: 0.625rem;
+	background-color: #fff;
+	li {
+		width: 68px;
+		height: 30px;
+		line-height: 30px;
+		text-align: center;
+		border-radius: 2px;
+		margin: 8px;
+		background-color: #f6f6f6;
+		color: #646464;
+		cursor: pointer;
+	}
+	.active {
+		background-color: #e6f3ff;
+		color: #0c89ff;
+	}
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,90 @@
+//模拟数据
+const express = require('express')
+const app = express()
+//推荐数据
+var recommendList = require('./src/data/recommend.json')
+//API路径
+var apiRoutes = express.Router()
+app.use('/api', apiRoutes)
+
 module.exports = {
-	devServer:{
-		port: 9090
+	devServer: {
+		host: "localhost",
+		port: 9090,
+		https: false,
+		open: true,
+		hotOnly: true,
+		before(app) {
+			app.get('/api/recommend', (req, res) => {
+				res.json({
+					code: 1,
+					msg: '成功',
+					data: recommendList
+				})
+			})
+			// app.get('/api/follow', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: followList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/total', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: totalList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/digital', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: digitalList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/film', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: filmList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/science', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: scienceList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/sport', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: sportList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/car', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: carList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/fashion', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: fashionList
+			// 	})
+			// })
+			// app.get('/api/hot-lists/school', (req, res) => {
+			// 	res.json({
+			// 		code: 1,
+			// 		msg: '成功',
+			// 		data: schoolList
+			// 	})
+			// })
+		}
 	}
 }
